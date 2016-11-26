efe8394e7a80925b20825fc84925a6ec7616bdf7
Baskara Patria <baskara@go-jek.com>
2016-11-26 15:54:22 +0700

    day 1

---
+++ app/src/main/java/com/gojek/justjava/MainActivity.java
@@ -0,0 +1,85 @@
+package com.gojek.justjava;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.AppCompatCheckBox;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final int DEFAULT_PRICE = 10000;
+    private static final int WHIPPED_CREAM_PRICE = 200;
+    private static final int CHOCOLATE_PRICE = 400;
+
+    private int itemQtt;
+    private TextView textItemQtt;
+    private Button buttonPlus;
+    private Button buttonMinus;
+    private Button buttonOrder;
+    private AppCompatCheckBox checkBoxItem1;
+    private AppCompatCheckBox checkBoxItem2;
+    private EditText inputItemName;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initViews();
+
+        itemQtt = 0;
+        textItemQtt.setText(String.valueOf(itemQtt));
+
+        buttonPlus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                itemQtt++;
+                textItemQtt.setText(String.valueOf(itemQtt));
+            }
+        });
+
+        buttonMinus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (itemQtt > 0) {
+                    itemQtt--;
+                    textItemQtt.setText(String.valueOf(itemQtt));
+                }
+            }
+        });
+        buttonOrder.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                countOrder();
+            }
+        });
+    }
+
+    private void initViews() {
+        textItemQtt = (TextView) findViewById(R.id.text_item_qtt);
+        buttonPlus = (Button) findViewById(R.id.button_plus);
+        buttonMinus = (Button) findViewById(R.id.button_minus);
+        buttonOrder = (Button) findViewById(R.id.button_order);
+        checkBoxItem1 = (AppCompatCheckBox) findViewById(R.id.check_item1);
+        checkBoxItem2 = (AppCompatCheckBox) findViewById(R.id.check_item2);
+        inputItemName = (EditText) findViewById(R.id.input_item_name);
+    }
+
+    private void countOrder() {
+        String itemName = inputItemName.getText().toString();
+
+        int price = DEFAULT_PRICE * itemQtt;
+        if (checkBoxItem1.isChecked()) {
+            price += WHIPPED_CREAM_PRICE;
+        }
+        if (checkBoxItem2.isChecked()) {
+            price += CHOCOLATE_PRICE;
+        }
+
+        Toast.makeText(this, "Harga: " + itemName + ": Rp " + price, Toast.LENGTH_SHORT).show();
+    }
+}
