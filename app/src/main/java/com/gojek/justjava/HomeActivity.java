edef8fe35603afeff09905f366aae818c642f661
Baskara Patria <baskara@go-jek.com>
2016-11-27 08:48:11 +0700

    create list in home activity

--- app/src/main/java/com/gojek/justjava/HomeActivity.java
+++ app/src/main/java/com/gojek/justjava/HomeActivity.java
@@ -2,12 +2,34 @@ package com.gojek.justjava;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class HomeActivity extends AppCompatActivity {
+
+    List<String> itemList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        RecyclerView listItem = (RecyclerView) findViewById(R.id.list_item_to_show);
+
+        createList();
+
+        HomeAdapter adapter = new HomeAdapter(itemList);
+        listItem.setLayoutManager(new LinearLayoutManager(this));
+        listItem.setAdapter(adapter);
+    }
+
+    private void createList() {
+        itemList = new ArrayList<>();
+        for (int i = 0; i < 50; i++) {
+            itemList.add("Item no. " + (i + 1));
+        }
     }
 }
